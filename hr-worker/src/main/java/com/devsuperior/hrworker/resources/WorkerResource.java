d49194922ec29daeeeb3d855da5903640fd5af6a
Leonardo Maia <leof.maia88@gmail.com>
2022-10-07 10:12:48 -0300

    hystrix fault tolerance

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -27,13 +27,21 @@ public class WorkerResource {
     private WorkerRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<Worker>> findAll(){
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id){
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        /*
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         */
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
